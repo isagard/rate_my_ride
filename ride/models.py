4b4eb84ed7b8bb9ee277164aa04ce4fc5fa97c59
“Fraser <2653099e@student.gla.ac.uk>
2023-03-09 17:22:13 +0000

    Models No Log In

--- ride/models.py
+++ ride/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Review(models.Model):
+
+	serviceID = models.ForeignKey(Service, on_delete=models.CASCADE) 
+	userID = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+	reviewID = models.AutoField(primary_key=True)
+	date = models.DateTimeField(auto_now_add=True)
+
+	location = models.CharField(max_length=32)
+	service = models.CharField(max_length=32)
+	rating = modles.IntegerField(default=0)
+	title = models.CharField(max_length=128)
+	body = models.CharField(max_length=256)
+
+	def __str__(self): 
+		return self.name
+
+class ServicePage(models.Model):
+
+	serviceID = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=32)
+	location = models.CharField(max_length=32)
+	body = models.CharField(max_length=256)
+	logo = models.ImageField(upload_to='logo_images', blank=True)
+
+	def __str__(self): 
+		return self.name
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+
+	def __str__(self):
+		return self.user.username
