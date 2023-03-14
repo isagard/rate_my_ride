fd35ddb76fcaa74fd31a1c0025ce187b70381cd2
Nawzad Haddad <2646194H@student.gla.ac.uk>
2023-03-14 15:33:37 +0000

    login half way

--- ride/admin.py
+++ ride/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from ride.models import Review, ServicePage
+from ride.models import UserProfile 
+
+class ServicePageAdmin(admin.ModelAdmin):
+    list_display = ('serviceID','name', 'location', 'body')
+
+class ReviewAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'serviceID', 'userID', 'reviewID', 'date', 'location', 'service', 'rating', 'title', 'body'}
 
 # Register your models here.
+admin.site.register(ServicePage, ServicePageAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(UserProfile)
